1ed6ed110e009a4ce27e83c15a6c218126885be3
Jiatu Liu <jiatu@opendoor.com>
2017-11-26 00:48:15 -0800

    Switch to CSS animation for Puree movements

--- js/puree.js
+++ js/puree.js
@@ -1,16 +1,15 @@
-// Move Puree across the screen in an infinite loop.
-function loop() {
-  $('.puree').css({
-    right: '-25%'
-  }).animate({
-    right: '+=125%'
-  }, {
-    duration: 8000,
-    easing: 'linear',
-    complete: loop
-  });
-}
-
 $(() => {
-  loop();
+  const puree = $('.puree');
+  $('#sit').hover(
+    () => {
+      puree.css({
+        'animation-play-state': 'paused'
+      });
+    },
+    () => {
+      puree.css({
+        'animation-play-state': 'running'
+      });
+    }
+    );
 });
