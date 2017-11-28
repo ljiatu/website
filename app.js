2a2276363b8506e3e7315f2b1668fbf37da2e6b7
Jiatu Liu <jiatu@opendoor.com>
2017-11-28 00:29:20 -0800

    Fix favicon and Puree link space

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const favicon = require('serve-favicon');
-const path = require('path');
 const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'pug');
 app.use('/static', express.static('static'));
-app.use(favicon(path.join(__dirname, 'static', 'images', 'favicon.ico')));
 
 app.get('/', (req, res) => {
   res.render('index');
