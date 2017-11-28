3eeb57abdf8992dd561d0464e1b43785c9650886
Jiatu Liu <jiatu@opendoor.com>
2017-11-28 00:00:39 -0800

    Use SSL, fix Opendoor underline

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const favicon = require('serve-favicon');
 const path = require('path');
+const sslRedirect = require('heroku-ssl-redirect');
 const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'pug');
 app.use('/static', express.static('static'));
 app.use(favicon(path.join(__dirname, 'static', 'images', 'favicon.ico')));
+app.use(sslRedirect());
 
 app.get('/', (req, res) => {
   res.render('index');
